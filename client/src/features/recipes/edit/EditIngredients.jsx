082c4950fe30af102075a71ba56a6eafe50435ac
Galo Sandoval <galo.sandoval@ibm.com>
2021-09-13 16:52:10 -0700

    adds edit functionality to instructions, adds delete instruction component

--- client/src/features/recipes/edit/EditIngredients.jsx
+++ client/src/features/recipes/edit/EditIngredients.jsx
@@ -1,24 +1,66 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-export const EditIngredients = ({ editIngredients, ingredients, recipe }) => {
+const initialAddState = { open: false, class: "add-ingredient input" };
+
+export const EditIngredients = ({ editIngredients, ingredients, recipe, getRecipeIngredients }) => {
   const [form, setForm] = useState([]);
-  // TODO: make a Put for ingredients by recipe id
+  const [ingredientToAdd, setIngredientToAdd] = useState("");
+  const [add, setAdd] = useState(initialAddState);
+  
   const handleChange = (event, index) => {
-    let tempForm = [...form];
-    let tempIngredient = { ...tempForm[index] };
-    tempIngredient.name = event.target.value;
-    tempForm[index] = tempIngredient;
-    setForm(tempForm);
+    const { name, value } = event.target;
+    if (name === "edit") {
+      let tempForm = [...form];
+      let tempIngredient = { ...tempForm[index] };
+      tempIngredient.name = value;
+      tempForm[index] = tempIngredient;
+      setForm(tempForm);
+    } else if (name === "add-ingredient") {
+      let tempIngredient = [...ingredientToAdd];
+      let tempObj = { ...tempIngredient[0] };
+      tempObj.name = value;
+      tempIngredient[0] = tempObj;
+      setIngredientToAdd(tempIngredient);
+    }
   };
 
-  const handleSubmit = () => {
-    axios
-      .put(`http://localhost:4000/ingredients/recipe/${recipe.id}`, form)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((error) => console.log(error.message));
+  const handleClick = () => {
+    add.open
+      ? setAdd(initialAddState)
+      : setAdd({ open: true, class: "add-ingredient input show-input" });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { name } = document.activeElement;
+    
+    if (name === "edit") {
+      axios
+        .put(`http://localhost:4000/ingredients/recipe/${recipe.id}`, form)
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((error) => console.log(error.message));
+    } else if (name === "add") {
+      const postBody = [
+        {
+          "recipe-id": recipe.id,
+          name: ingredientToAdd,
+          price: 100,
+          measurement: "each",
+          amount: 1
+        }
+      ];
+      axios
+        .post("http://localhost:4000/ingredients/", postBody)
+        .then((res) => {
+          console.log(res.data);
+          getRecipeIngredients(recipe.id);
+          setIngredientToAdd("");
+        })
+        .catch((error) => console.log(error.message));
+    }
   };
 
   useEffect(() => {
@@ -34,9 +76,31 @@ export const EditIngredients = ({ editIngredients, ingredients, recipe }) => {
               key={ingredient.id}
               value={ingredient.name}
               onChange={(event) => handleChange(event, index)}
+              name="edit"
             />
           ))}
-        <button type="submit">Save Changes</button>
+        <div className={add.class}>
+          <input
+            type="text"
+            value={ingredientToAdd}
+            onChange={(event) => setIngredientToAdd(event.target.value)}
+            name="add-ingredient"
+            className="input"
+            placeholder="Add an ingredient"
+          />
+        </div>
+        {add.open ? (
+          <button name="add" type="submit">
+            Add
+          </button>
+        ) : (
+          <button name="edit" type="submit">
+            Save Changes
+          </button>
+        )}
+        <button name="add-btn" onClick={handleClick}>
+          {add.open ? "Cancel" : "+"}
+        </button>
       </form>
     </div>
   );
--- client/src/features/recipes/edit/EditInstructions.jsx
+++ client/src/features/recipes/edit/EditInstructions.jsx
@@ -1,24 +1,79 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { DeleteInstruction } from "../delete/DeleteInstruction";
 
-export const EditInstructions = ({ editInstructions, instructions }) => {
+const addInitialState = {
+  open: false,
+  class: "add-instruction input"
+};
+
+const instructionInitialState = [
+  {
+    "recipe-id": null,
+    description: "",
+    step: null
+  }
+];
+
+export const EditInstructions = ({
+  editInstructions,
+  instructions,
+  getRecipeInstructions,
+  recipe
+}) => {
   const [form, setForm] = useState([]);
+  const [instructionToAdd, setInstructionToAdd] = useState(instructionInitialState);
+  const [add, setAdd] = useState(addInitialState);
 
   const handleChange = (event, index) => {
-    let tempForm = [...form];
-    let tempInstruction = { ...tempForm[index] };
-    tempInstruction.description = event.target.value;
-    tempForm[index] = tempInstruction;
-    setForm(tempForm);
+    const { name, value } = event.target;
+    if (name === "edit-description") {
+      let tempForm = [...form];
+      let tempInstruction = { ...tempForm[index] };
+      tempInstruction.description = value;
+      tempForm[index] = tempInstruction;
+      setForm(tempForm);
+    }
+    if (name === "add-instruction") {
+      let tempForm = [...instructionToAdd];
+      let tempObj = { ...tempForm[0] };
+      tempObj.description = value;
+      tempObj["recipe-id"] = recipe.id;
+      tempObj.step = form.length + 1;
+      tempForm[0] = tempObj;
+      setInstructionToAdd(tempForm);
+    }
   };
 
-  const handleSubmit = () => {
-    axios
-      .put(`http://localhost:4000/instructions/${form[0]["recipe-id"]}`, form)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((error) => console.log(error.message));
+  const handleClick = (event) => {
+    event.preventDefault();
+    add.open
+      ? setAdd(addInitialState)
+      : setAdd({ open: true, class: "add-instruction input show-input" });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { name } = document.activeElement;
+
+    if (name === "edit") {
+      axios
+        .put(`http://localhost:4000/instructions/${form[0]["recipe-id"]}`, form)
+        .then((res) => {
+          console.log("edit", res.data);
+          getRecipeInstructions(form[0]["recipe-id"]);
+        })
+        .catch((error) => console.log(error.message));
+    } else if (name === "add") {
+      axios
+        .post("http://localhost:4000/instructions/", instructionToAdd)
+        .then((res) => {
+          console.log(res.data);
+          getRecipeInstructions(form[0]["recipe-id"]);
+          setInstructionToAdd(instructionInitialState);
+        })
+        .catch((error) => console.log("error", error));
+    }
   };
 
   useEffect(() => {
@@ -28,18 +83,44 @@ export const EditInstructions = ({ editInstructions, instructions }) => {
   return (
     <div className={editInstructions.class}>
       <form className="edit-instructions-form" onSubmit={handleSubmit}>
-        {form &&
-          form.map((instruction, index) => (
-            <input
-              className="input"
-              type="text"
-              key={instruction.id}
-              value={instruction.description}
-              onChange={(event) => handleChange(event, index)}
-              name="description"
-            />
-          ))}
-        <button type="submit">Save Changes</button>
+        <div className="instructions">
+          {form &&
+            form.map((instruction, index) => (
+              <>
+                <input
+                  className="input"
+                  type="text"
+                  key={instruction.id}
+                  value={instruction.description}
+                  onChange={(event) => handleChange(event, index)}
+                  name="edit-description"
+                />
+                <DeleteInstruction instruction={instruction} />
+              </>
+            ))}
+        </div>
+        <div className={add.class}>
+          <input
+            type="text"
+            value={instructionToAdd[0].description}
+            onChange={handleChange}
+            name="add-instruction"
+            className="input"
+            placeholder="Add an instruction"
+          />
+        </div>
+        {add.open ? (
+          <button name="add" type="submit">
+            Add
+          </button>
+        ) : (
+          <button name="edit" type="submit">
+            Save Changes
+          </button>
+        )}
+        <button name="add-btn" onClick={handleClick}>
+          {add.open ? "Cancel" : "+"}
+        </button>
       </form>
     </div>
   );
