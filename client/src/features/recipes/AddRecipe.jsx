f7074c94e7295b12d484929abbd0141b60d7bc5e
Galo Sandoval <galo.sandoval@ibm.com>
2021-09-21 15:33:49 -0700

    first draft of instructions parse functionality finished

--- client/src/features/recipes/AddRecipe.jsx
+++ client/src/features/recipes/AddRecipe.jsx
@@ -19,26 +19,61 @@ export const AddRecipe = ({ recipes }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
     const recipeBody = {
       "recipe-name": recipeToAdd.name,
       description: recipeToAdd.description,
-      "user-id": recipes.id,
+      // TODO: Hard coded with 1, replace with user-id
+      "user-id": 1,
       "img-url": recipeToAdd.imageUrl
     };
+
+    let newLine = "STEP ";
+    console.log("newline", newLine.split("\n"));
+    let instructionsBody = recipeToAdd.instructions.split(" ");
+    let instructions = [];
+    let instruction = [];
+    let count = 0;
+    for (let i = 0; i < instructionsBody.length + count; i++) {
+      if (instructionsBody[i]?.includes("\n")) {
+        let toAdd = instructionsBody[i].split("\n");
+
+        instruction.push(toAdd[0]);
+
+        if (instruction.length > 2) instructions.push(instruction);
+
+        instruction = [];
+
+        if (toAdd.length === 3) instruction.push(toAdd[2]);
+        else if (toAdd.length === 4) instruction.push(toAdd[3]);
+        else instruction.push(toAdd[1]);
+
+        count++;
+        continue;
+      }
+
+      if (instructionsBody[i] !== undefined) instruction.push(instructionsBody[i]);
+
+      if (instructionsBody.length === i) instructions.push(instruction);
+    }
+
     // TODO: Figure out how to add a recipeID to the recipe body
-    let recipeId;
-    axios
-      .post("http://localhost:4000/recipes/", recipeBody)
-      .then((recipeAdded) => {
-        console.log(recipeAdded);
-        recipeId = recipeToAdd.id;
-      })
-      .then(() => {
-        axios.post("");
-      });
+
+    console.log("instruction body", instructionsBody);
+    console.log("instruction body", instructions);
+    // axios
+    //   .post("http://localhost:4000/recipes/", recipeBody)
+    //   .then((recipeAdded) => {
+    //     console.log(recipeAdded.data.recipe[0]);
+    //     recipeId = recipeAdded.data.recipe[0];
+    //   })
+    //   .then(() => {
+    //     console.log(recipeId);
+    //     axios.post("http://localhost:4000/instructions/", instructionsBody);
+    //   });
   };
   return (
-    <div className="add-recipe" onSubmit={handleSubmit}>
+    <form className="add-recipe" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Recipe Name"
@@ -71,6 +106,7 @@ export const AddRecipe = ({ recipes }) => {
         value={recipeToAdd.instructions}
         onChange={handleChange}
       />
-    </div>
+      <button type="submit">Add Recipe</button>
+    </form>
   );
 };
--- client/src/features/recipes/Recipes.jsx
+++ client/src/features/recipes/Recipes.jsx
@@ -23,7 +23,7 @@ export const Recipes = ({ recipes }) => {
   return (
     <div className="recipes" onClick={handleClick}>
       <h1>Recipes</h1>
-      <AddRecipe recipes={recipes}/>
+      <AddRecipe recipes={recipes} />
       <div className="recipes-container">
         {recipes.map((recipe, index) => (
           <RecipeCard
