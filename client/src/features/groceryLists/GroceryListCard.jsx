3ebf32d58f8a79f31f9aaa8d23caf0a82e8ad9e6
Galo Sandoval <galo.sandoval@ibm.com>
2021-10-14 15:35:26 -0700

    adds styling to carousel

--- client/src/features/groceryLists/GroceryListCard.jsx
+++ client/src/features/groceryLists/GroceryListCard.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import "../../styles/recipesStyles.css";
 import "../../styles/grocerylistStyles.css";
 
 export const GroceryListCard = ({ list }) => {
   const [carousel, setCarousel] = useState(0);
-  const [leftButton, setLeftButton] = useState(2);
-  const [rightButton, setRightButton] = useState(2);
   const [page, setPage] = useState(1);
-  console.log("page", page);
-  console.log("list", list);
+
   //  const list = {
   //    completed: 0
   //    created_at: "2021-10-13 21:52:38"
@@ -24,49 +21,74 @@ export const GroceryListCard = ({ list }) => {
   //   }
 
   const handleClick = (event) => {
-    const { name } = event.target;
+    const { name } = event.currentTarget;
     if (name === "right-button") {
       setCarousel((state) => (state -= 25));
-      setLeftButton((state) => (state += 30));
-      setRightButton((state) => (state -= 30));
       setPage((state) => (state += 1));
     }
     if (name === "left-button") {
       setCarousel((state) => (state += 25));
-      setLeftButton((state) => (state -= 30));
-      setRightButton((state) => (state += 30));
       setPage((state) => (state -= 1));
     }
   };
 
+  useLayoutEffect(() => {
+    const circles = document.querySelectorAll(".circle-svg");
+    if (circles[page - 2]) {
+      circles[page - 2].classList.remove("no-opacity");
+    } else {
+      circles[page].classList.remove("no-opacity");
+    }
+    circles[page - 1].classList.add("no-opacity");
+  }, [page]);
+
   return (
     <div className="card" key={list.id} onClick={handleClick}>
       <h2>{list["grocery-list-name"]}</h2>
-      <div className="images-container" style={{ transform: `translateX(${carousel}em)` }}>
+      <div className="images-container">
         {list["img-url"].length > 1 && (
           <>
-            <button
-              style={{ left: `${leftButton}em`, display: page === 1 ? "none" : null }}
-              name="left-button"
-              className="images-button left"
-            >
-              {"<"}
-            </button>
-            <button
-              style={{
-                right: `${rightButton}em`,
-                display: page === list["img-url"].length ? "none" : null
-              }}
-              name="right-button"
-              className="images-button right"
-            >
-              {">"}
-            </button>
+            <div className="buttons-container">
+              {page !== 1 ? (
+                <button name="left-button" className="images-button left" onClick={handleClick}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white">
+                    <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
+                  </svg>
+                </button>
+              ) : (
+                <span></span>
+              )}
+              {page !== list["img-url"].length ? (
+                <button name="right-button" className="images-button right" onClick={handleClick}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white">
+                    <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
+                  </svg>
+                </button>
+              ) : (
+                <span></span>
+              )}
+            </div>
+            <div className="circle-container">
+              {[...Array(list["img-url"].length)].map((_element, index) => (
+                <svg
+                  key={index}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  className={`circle-svg circle${index}`}
+                >
+                  <circle cx="12" cy="12" r="12" />
+                </svg>
+              ))}
+            </div>
           </>
         )}
-        {list["img-url"].map((img, index) => {
-          return <img src={img} alt={list.descriptions} key={list.description[index]} />;
-        })}
+        <div className="images" style={{ transform: `translateX(${carousel}em)` }}>
+          {list["img-url"].map((img, index) => {
+            return <img src={img} alt={list.descriptions} key={list.description[index]} />;
+          })}
+        </div>
       </div>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo fuga corporis modi pariatur
