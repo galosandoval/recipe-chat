58b8f838a9ac35eca53499921e0645ac4251aa62
Galo Sandoval <galosan92@gmail.com>
2021-11-25 16:21:19 -0800

    adds auth router and creates a user

--- server/api/auth/auth-router.js
+++ server/api/auth/auth-router.js
@@ -1,9 +1,19 @@
 const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const Users = require("../users/users-model");
 
-router.post("/register", (req, res) => {
-  const creds = req.body;
+router.post("/register", async (req, res) => {
+  const { username, password: plainPassword } = req.body;
 
-  console.log({ creds });
+  const password = await bcryptjs.hash(plainPassword, 10);
+
+  try {
+    const response = await Users.addUser({ username, password });
+    console.log("Created new user", response);
+  } catch (error) {
+    console.log(error);
+    return res.json({ status: "error" });
+  }
 
   res.json({ status: "ok" });
 });
--- server/api/users/users-model.js
+++ server/api/users/users-model.js
@@ -2,9 +2,17 @@ const db = require("../../data/connection");
 
 const findUsers = () => db("users");
 
-const findUserById = (id) => db("users").where("id", id);
+const findUserById = (id) => db("users").where({ id });
+
+const addUser = (creds) =>
+  db("users")
+    .insert(creds)
+    .then((newUserId) => {
+      return findUserById(newUserId);
+    });
 
 module.exports = {
   findUsers,
-  findUserById
+  findUserById,
+  addUser
 };
