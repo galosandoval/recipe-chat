1e7defb3ba867e5b30a7a9dbb17ca1c086005260
Galo Sandoval <galosan92@gmail.com>
2021-12-02 22:23:18 -0800

    refactoring register

--- server/api/auth/auth-router.js
+++ server/api/auth/auth-router.js
@@ -3,6 +3,8 @@ const bcryptjs = require("bcryptjs");
 const Users = require("../users/users-model");
 const { validateBody } = require("./auth-middleware");
 const jwt = require("jsonwebtoken");
+const data = require("../../data/seed-data");
+const db = require("../../data/connection");
 
 router.post("/register", validateBody, async (req, res) => {
   const { username, password: plainPassword } = req.body;
@@ -12,6 +14,33 @@ router.post("/register", validateBody, async (req, res) => {
     const addedUser = await Users.addUser({ username, password });
     const token = makeJwt(addedUser[0]);
 
+    const recipesToAdd = data.recipes;
+    recipesToAdd.forEach((recipe) => (recipe["user-id"] = addedUser[0].id));
+    const grocerylistsToAdd = data.grocerylists;
+    grocerylistsToAdd.forEach((gl) => (gl["user-id"] = addedUser[0].id));
+
+    const recipesAdded = await db("recipes").insert(recipesToAdd);
+    await db("grocery-lists").insert(grocerylistsToAdd);
+
+    const ingredientsToAdd = data.ingredients;
+    ingredientsToAdd.forEach((i, index) => {
+      if (index < 14) i["recipe-id"] = recipesAdded[0] - 3;
+      else if (index >= 14 && index < 19) i["recipe-id"] = recipesAdded[0] - 2;
+      else if (index >= 19 && index < 36) i["recipe-id"] = recipesAdded[0] - 1;
+      else i["recipe-id"] = recipesAdded[0];
+    });
+
+    const instructionsToAdd = data.instructions;
+    instructionsToAdd.forEach((i, index) => {
+      if (index < 3) i["recipe-id"] = recipesAdded[0] - 3;
+      else if (index >= 3 && index < 5) i["recipe-id"] = recipesAdded[0] - 2;
+      else if (index >= 5 && index < 10) i["recipe-id"] = recipesAdded[0] - 1;
+      else i["recipe-id"] = recipesAdded[0];
+    });
+
+    
+
+    
     res.status(201).json({ status: "ok", user: addedUser[0], token });
   } catch (error) {
     if (error.code === "SQLITE_CONSTRAINT") {
