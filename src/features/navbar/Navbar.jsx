cfb20d863e97f22c4d5c86a99f30dae738e28528
Galo Sandoval <galosan92@gmail.com>
2021-12-07 19:58:47 -0800

    finishes open sidebar

--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -1,11 +1,51 @@
 import React, { useEffect, useState } from "react";
-import { menuSVG } from "../../styles/svgs";
+import { menuSVG, xSVG } from "../../styles/svgs";
 import { debounce } from "../utils/debounce";
 import { NavLinks } from "./NavLinks";
+import { Sidebar } from "./Sidebar";
+
+const initialSidebarState = {
+  transform: "translateY(-100%)"
+};
 
 export const Navbar = () => {
   const [visible, setVisible] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [sidebarStyle, setSidebarStyle] = useState(initialSidebarState);
+
+  const handleSidebar = () => {
+    // const modal = document.querySelector("body");
+    // const sidebar = document.querySelector(".navbar__sidebar");
+    // if (sidebarVisible) {
+    //   modal.classList.remove("modal-blur");
+    //   sidebar.classList.add("navbar__sidebar--hidden");
+    // } else {
+    //   sidebar.classList.remove("navbar__sidebar--hidden");
+    //   modal.classList.add("modal-blur");
+    // }
+
+    const circles = document.querySelectorAll(".carousel__circles");
+    const arrows = document.querySelectorAll(".carousel__buttons");
+
+    console.log({ circles });
+
+    if (sidebarVisible) {
+      console.log("showem");
+      setTimeout(() => {
+        circles.forEach((c) => (c.style.zIndex = 1));
+        arrows.forEach((c) => (c.style.zIndex = 1));
+      }, 800);
+      setSidebarVisible(false);
+      setSidebarStyle(initialSidebarState);
+    } else {
+      console.log("dont showem");
+      circles.forEach((c) => (c.style.zIndex = 0));
+      arrows.forEach((c) => (c.style.zIndex = 0));
+      setSidebarVisible(true);
+      setSidebarStyle({ transform: "translateY(0)" });
+    }
+  };
 
   const handleScroll = debounce(() => {
     const currentScrollPos = window.pageYOffset;
@@ -30,9 +70,14 @@ export const Navbar = () => {
         <ul className="navbar__list navbar__list--default">
           <NavLinks />
         </ul>
-        <ul className="navbar__list navbar__list--phone">
-          <button className="navbar__list-btn">{menuSVG}</button>
-        </ul>
+        <div className="navbar__list navbar__list--phone">
+          <input type="checkbox" className="navbar__checkbox" id="nav-toggle" />
+          <label onClick={handleSidebar} className="navbar__list-btn" htmlFor="nav-toggle">
+            {sidebarVisible ? xSVG : menuSVG}
+          </label>
+          <div className="navbar__background">{""}</div>
+        </div>
+        <Sidebar sidebarStyle={sidebarStyle} />
       </div>
     </>
   );
--- src/features/recipe/delete/DeleteItem.jsx
+++ src/features/recipe/delete/DeleteItem.jsx
@@ -4,9 +4,9 @@ import { deleteSVG } from "../../../styles/svgs";
 export const DeleteItem = ({ setToBeDeleted, item, setDeleteModal }) => {
   const openDeleteModal = () => {
     const modal = document.querySelector("body");
+    modal.classList.add("modal-blur");
 
     setToBeDeleted(item);
-    modal.classList.add("modal-blur");
     setDeleteModal({
       isOpen: true,
       className: "delete-confirmation"
