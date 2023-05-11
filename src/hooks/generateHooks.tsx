2457e8ba81b5e30d639eea6585741586ca6d26b2
Galo Sandoval <galosan92@gmail.com>
2023-05-10 18:29:20 -0700

    reorganizes _generate

---
+++ src/hooks/generateHooks.tsx
@@ -0,0 +1,127 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
+import { FormValues } from 'pages/_generate'
+import { MouseEvent, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { GeneratedRecipe } from 'server/api/routers/recipe/interface'
+import { api } from 'utils/api'
+import { z } from 'zod'
+
+export const useGeneratedRecipe = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty, isValid },
+    setValue,
+    clearErrors
+  } = useForm<GenerateRecipeParams>({
+    resolver: zodResolver(generateRecipeFormSchema)
+  })
+  const [isGenRecipeOpen, setIsGenRecipeOpen] = useState(false)
+  const [enableCloseModal, setEnableCloseModal] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const genRecipe = api.recipe.generate.useMutation()
+
+  const onSubmit = async (values: GenerateRecipeParams) => {
+    setIsGenRecipeOpen(true)
+    genRecipe.mutate(values)
+  }
+
+  const handleCloseModal = () => {
+    setIsGenRecipeOpen(false)
+  }
+
+  const handleFillMessage = (e: MouseEvent<HTMLButtonElement>) => {
+    setValue('message', e.currentTarget.innerText.toLowerCase(), {
+      shouldValidate: true,
+      shouldDirty: true
+    })
+    clearErrors()
+  }
+
+  const handleEnableCloseModal = () => {
+    setEnableCloseModal(true)
+  }
+
+  // const addMessage = (values: GenerateRecipeParams) => {
+  //   setMessages((state) => [
+  //     ...state,
+  //     { from: 'me', timeStamp: new Date().toISOString(), value: values.message }
+  //   ])
+  //   reset()
+
+  //   setTimeout(() => {
+  //     setMessages((state) => [
+  //       ...state,
+  //       {
+  //         from: 'chat',
+  //         timeStamp: new Date().toISOString(),
+  //         value: 'Generating your recipe...'
+  //       }
+  //     ])
+  //   }, 500)
+  // }
+
+  return {
+    enableCloseModal,
+    genRecipe,
+    isDirty,
+    isGenRecipeOpen,
+    isValid,
+    messages,
+    handleCloseModal,
+    handleEnableCloseModal,
+    handleFillMessage,
+    handleSubmit: () => handleSubmit(onSubmit),
+    register
+  }
+}
+
+const generateRecipeFormSchema = z.object({ message: z.string().min(6) })
+export type GenerateRecipeParams = z.infer<typeof generateRecipeFormSchema>
+
+type Message = {
+  from: 'me' | 'chat'
+  value: string
+  timeStamp: string
+}
+
+export const useCreateGeneratedRecipe = (data: GeneratedRecipe) => {
+  const router = useRouter()
+  const utils = api.useContext()
+  const { description, ingredients, instructions, name, cookTime, prepTime } =
+    data
+  const { mutate, isLoading, isSuccess } = api.recipe.create.useMutation({
+    onSuccess: (data) => {
+      router.push(`recipes/${data.id}?name=${encodeURIComponent(data.name)}`)
+      utils.recipe.invalidate()
+    }
+  })
+
+  const { handleSubmit, register, getValues } = useForm<FormValues>({
+    defaultValues: {
+      description,
+      ingredients: ingredients.join('\n'),
+      instructions: instructions.join('\n'),
+      name,
+      cookTime,
+      prepTime
+    }
+  })
+
+  const onSubmit = (values: FormValues) => {
+    const ingredients = values.ingredients.split('\n')
+    const instructions = values.instructions.split('\n')
+    mutate({ ...values, ingredients, instructions })
+  }
+
+  return {
+    isLoading,
+    isSuccess,
+    mutate,
+    getValues,
+    handleSubmit: () => handleSubmit(onSubmit),
+    register
+  }
+}
