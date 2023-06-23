1f607270ee9dd95cef7fefa10bc7bb8323fd6724
Galo Sandoval <galosan92@gmail.com>
2023-06-23 17:48:55 -0600

    removes uploadthing

--- src/pages/api/uploadthing.ts
+++
@@ -1,8 +0,0 @@
-import { myFileRouter } from 'server/uploadthing'
-import { createNextPageApiHandler } from 'uploadthing/next-legacy'
-
-const handler = createNextPageApiHandler({
-  router: myFileRouter
-})
-
-export default handler
--- src/utils/uploadthing.ts
+++
@@ -1,5 +0,0 @@
-import { generateReactHelpers } from '@uploadthing/react/hooks'
-import { MyFileRouter } from 'server/uploadthing'
-
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<MyFileRouter>()
