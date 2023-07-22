c174331e390971062ceb70b2d780634247b1403c
Galo Sandoval <galosan92@gmail.com>
2023-07-21 18:53:40 -0600

    fixes desktop sizing for chat message

--- src/components/loaders/ChatLoader.tsx
+++ src/components/loaders/ChatLoader.tsx
@@ -3,7 +3,7 @@ import { UserCircleIcon } from 'components/Icons'
 export const ChatLoader = () => {
   return (
     <div className='bg-primary-content py-4 pb-4 pl-4'>
-      <div className='prose flex justify-start gap-2'>
+      <div className='prose mx-auto flex justify-start gap-2'>
         <UserCircleIcon />
         <div className='flex items-center justify-start space-x-1'>
           <div
