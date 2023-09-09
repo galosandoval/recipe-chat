81b6ff0503c3e729e008131133f1ce484fd8c023
Galo Sandoval <galosan92@gmail.com>
2023-09-09 12:27:02 -0600

    styling and loading spinner on infinite recipes

--- src/components/loaders/ScreenLoader.tsx
+++ src/components/loaders/ScreenLoader.tsx
@@ -1,7 +1,11 @@
 export function ScreenLoader() {
   return (
     <div className='grid h-[calc(100svh-64px)] place-items-center'>
-      <span className='loading loading-spinner text-primary'></span>
+      <LoadingSpinner />
     </div>
   )
+}
+
+export function LoadingSpinner() {
+  return <span className='loading loading-spinner text-primary'></span>
 }
