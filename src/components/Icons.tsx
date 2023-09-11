1fc6be9dddc7ce11048b11bda2dbb37246a22ba7
Galo Sandoval <galosan92@gmail.com>
2023-09-11 17:35:28 -0600

    build

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,20 +1,3 @@
-export const ListBulletIcon = ({ size }: { size?: number }) => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    fill='none'
-    viewBox='0 0 24 24'
-    strokeWidth={1.5}
-    stroke='currentColor'
-    className={`h-${size || 6} w-${size || 6}`}
-  >
-    <path
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      d='M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
-    />
-  </svg>
-)
-
 export const PlusIcon = ({ size }: { size?: number }) => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
@@ -28,6 +11,23 @@ export const PlusIcon = ({ size }: { size?: number }) => (
       strokeLinecap='round'
       strokeLinejoin='round'
       d='M12 4.5v15m7.5-7.5h-15'
+    />
+  </svg>
+)
+
+export const ListBulletIcon = ({ size }: { size?: number }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className={`h-${size || 6} w-${size || 6}`}
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
     />
   </svg>
 )
