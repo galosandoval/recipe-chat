bc98e71abd75c7447c5201d0f9b6834f60de4a17
Galo Sandoval <galosan92@gmail.com>
2023-10-12 22:34:41 -0600

    localePath added to fix bug

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'en'
-  }
+  },
+  localePath: require('path').resolve('./public/locales')
 }
