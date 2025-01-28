fae48753f9fc1ac37debbc77e2d56dcdf7b25b90
Galo Sandoval <galosan92@gmail.com>
2025-01-27 19:03:45 -0600

    model change to 4o

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -63,7 +63,7 @@ export default async function handler(req: NextRequest) {
   params.push(...messages)
 
   const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4o',
     stream: true,
     messages: params
   })
