e932f9b1bffe85e3e65253bdcd6f9c2558662bef
Galo Sandoval <galosan92@gmail.com>
2025-02-17 17:45:12 -0600

    changes usage

--- src/server/api/data-access/ingredients.ts
+++ src/server/api/data-access/ingredients.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from '@prisma/client'
-import { prisma } from '~/server/db'
 
-class IngredientsDataAccess {
-  constructor(readonly prisma: PrismaClient) {}
+export class IngredientsDataAccess {
+  constructor(private readonly prisma: PrismaClient) {}
 
   async deleteIngredientsByRecipeId(recipeId: string) {
     return await this.prisma.ingredient.deleteMany({ where: { recipeId } })
   }
 }
-
-export const ingredientsDataAccess = new IngredientsDataAccess(prisma)
