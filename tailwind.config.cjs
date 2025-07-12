a6d697d68e0160ba00bb58a5ebb071c75a3b1f0c
Galo Sandoval <galosan92@gmail.com>
2025-07-11 23:07:52 -0600

    wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        roboto: ['var(--font-roboto)']
-      }
-    }
-  },
-  plugins: [require('@tailwindcss/typography')],
   darkMode: 'class'
 }
