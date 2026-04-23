d106e926aee1fe635215d8b5050d034eef423454
Galo Sandoval <galosan92@gmail.com>
2026-04-22 21:47:26 -0600

    restructures cookingPhrases translations to keyed object and rewrites rotating phrases component

--- src/components/loaders/assistant-message.tsx
+++ src/components/loaders/assistant-message.tsx
@@ -2,18 +2,13 @@
 
 import { LoadingSpinner } from '~/components/loaders/loading-spinner'
 import { RotatingPhrases } from '~/components/loaders/rotating-phrases'
-import { useTranslations } from '~/hooks/use-translations'
 
 export function AssistantMessageLoader() {
-  const t = useTranslations()
-
   return (
     <div className='bg-transparent pb-4 pl-4'>
       <div className='mx-auto flex items-center justify-center gap-2'>
         <LoadingSpinner className='text-primary size-4 shrink-0' />
-        <RotatingPhrases
-          phrases={t.loaders.cookingPhrases as unknown as string[]}
-        />
+        <RotatingPhrases />
       </div>
     </div>
   )
